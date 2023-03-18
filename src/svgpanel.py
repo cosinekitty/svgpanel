de605f2fba39daa126562236e6fd7100b3d97f3e
Don Cross <cosinekitty@gmail.com>
2023-03-18 15:04:47 -0400

    Setting up unit tests and GitHub Actions.

---
+++ src/svgpanel.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""Classes for generating VCV Rack panel designs.
+"""
+
---
+++ src/unittest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+from svgpanel import *
+
+def RunTest() -> int:
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(RunTest())
