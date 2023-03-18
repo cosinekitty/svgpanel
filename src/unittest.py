8c59ebdc1cb15dad2ebfc194869bb893b58d6e69
Don Cross <cosinekitty@gmail.com>
2023-03-18 15:33:50 -0400

    Basic test harness is now in place.

--- src/unittest.py
+++ src/unittest.py
@@ -1,9 +1,53 @@
 #!/usr/bin/env python3
 import sys
+from typing import Any, List, Tuple, Optional, Union, Callable, Dict
 from svgpanel import *
 
-def RunTest() -> int:
+Verbose = False
+
+def Info(message: str) -> None:
+    print('unittest.py | {}'.format(message))
+
+def Debug(message: str) -> None:
+    if Verbose:
+        Info(message)
+
+def Nothing() -> int:
     return 0
 
+UnitTests: Dict[str, Callable[[], int]] = {
+    'nothing' : Nothing
+}
+
+def RunTest(args: List[str]) -> int:
+    if len(args) > 0:
+        if args[0] == '-v':
+            global Verbose
+            Verbose = True
+            args = args[1:]
+    if len(args) == 1:
+        name = args[0]
+        if name == 'all':
+            for (name, func) in UnitTests.items():
+                if func() == 0:
+                    Info('PASS: {}'.format(name))
+                else:
+                    Info('FAIL: {}'.format(name))
+                    return 1
+            Info('SUCCESS')
+            return 0
+        checkfunc = UnitTests.get(name)
+        if not checkfunc:
+            Info('ERROR: unknown test name [{}]'.format(name))
+            return 1
+        if checkfunc() == 0:
+            Info('PASS: {}'.format(name))
+        else:
+            Info('FAIL: {}'.format(name))
+            return 1
+    print('unittest.py : ERROR: invalid command line.')
+    return 1
+
+
 if __name__ == '__main__':
-    sys.exit(RunTest())
+    sys.exit(RunTest(sys.argv[1:]))
